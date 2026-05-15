eedb9d1a4f395b41c48545859a3cbc735d8f01d4
Sepul11 <simonsevelascos@gmail.com>
2026-05-14 22:42:54 -0500

    feature prueba

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -5,7 +5,7 @@ export default function Register() {
     <>
       <Navbar /> 
       <main className="p-6">
-        <h1 className="text-2xl font-bold">Registrate</h1>
+        <h1 className="text-2xl font-bold">Registro</h1>
         <p>Contenido de la página de registrarse.</p>
       </main>
     </>
